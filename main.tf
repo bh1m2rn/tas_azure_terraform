ff4cf14b56fd5bdcdb7faf00720712677ca820fb
pivotal-james-larrea <jlarrea@pivotal.io>
2021-03-27 23:29:40 -0400

    First commit - Deploy Opsman/Bosh director only

---
+++ main.tf
@@ -0,0 +1,372 @@
+#############################################################################
+# VARIABLES
+#############################################################################
+
+variable "resource_group_name" {
+  type    = string
+}
+
+variable "location" {
+  type    = string
+  default = "eastus"
+}
+
+variable "sp_identifier" {
+  type    = string
+}
+
+variable "opsman_image_url" {
+  type    = string
+}
+
+
+#############################################################################
+# DATA
+#############################################################################
+
+data "azurerm_subscription" "current" {}
+
+data "azurerm_public_ip" "opsman_ipaddress" {
+  name                = azurerm_public_ip.opsman_ip.name
+  resource_group_name = azurerm_public_ip.opsman_ip.resource_group_name
+}
+
+#############################################################################
+# PROVIDERS
+#############################################################################
+
+terraform {
+  required_providers {
+    azure = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.52.0"
+    }
+    ad = {
+      source  = "hashicorp/azuread"
+      version = "~> 1.4.0" 
+    }
+  }
+}
+
+provider "azure" {
+  features {}
+}
+
+#############################################################################
+# RESOURCES
+#############################################################################
+
+## AZURE AD SP ##
+
+resource "random_password" "sp_for_tas" {
+  length  = 16
+  special = true
+  override_special = "_%@"
+}
+
+resource "azuread_application" "sp_for_tas" {
+  display_name               = "Service Principal for BOSH"
+  homepage           = "http://BOSHAzureCPI"
+  identifier_uris    = [var.sp_identifier]
+}
+
+resource "time_sleep" "wait_2_mins" {
+  depends_on = [azuread_application.sp_for_tas]
+
+  create_duration = "2m"
+}
+
+resource "azuread_service_principal" "sp_for_tas" {
+  application_id     = azuread_application.sp_for_tas.application_id
+  depends_on = [time_sleep.wait_2_mins]
+}
+
+resource "azuread_service_principal_password" "sp_for_tas" {
+  service_principal_id = azuread_service_principal.sp_for_tas.id
+  value                = random_password.sp_for_tas.result
+  end_date_relative    = "720h"
+}
+
+resource "azurerm_role_assignment" "sp_for_tas" {
+  scope                = data.azurerm_subscription.current.id
+  role_definition_name = "Owner"
+  principal_id         = azuread_service_principal.sp_for_tas.id
+}
+
+
+## RESOURCE GROUPS ##
+
+resource "azurerm_resource_group" "rg_tas" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+
+## NETWORKING ##
+
+module "vnet" {
+  source              = "Azure/vnet/azurerm"
+  vnet_name           = "tas-virtual-network"
+  version             = "2.3.0"
+  resource_group_name = azurerm_resource_group.rg_tas.name
+  address_space       = ["10.0.0.0/16"]
+  subnet_prefixes     = ["10.0.4.0/26", "10.0.12.0/22", "10.0.8.0/22"]
+  subnet_names        = ["tas-infrastructure", "tas-runtime", "tas-services"]
+
+  nsg_ids = {
+    tas-infrastructure = azurerm_network_security_group.tas_nsg.id
+    tas-runtime = azurerm_network_security_group.tas_nsg.id
+    tas-services = azurerm_network_security_group.tas_nsg.id
+  }  
+
+  depends_on = [azurerm_resource_group.rg_tas]
+}
+
+resource "azurerm_network_security_group" "tas_nsg" {
+  name                = "tas_nsg"
+  resource_group_name = azurerm_resource_group.rg_tas.name
+  location            = azurerm_resource_group.rg_tas.location
+
+  security_rule {
+    name                       = "ssh"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "http"
+    priority                   = 200
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+    
+    security_rule {
+    name                       = "https"
+    priority                   = 300
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "443"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+    security_rule {
+    name                       = "diego-ssh"
+    priority                   = 400
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "2222"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_network_security_group" "opsman_nsg" {
+  name                = "opsman_nsg"
+  resource_group_name = azurerm_resource_group.rg_tas.name
+  location            = azurerm_resource_group.rg_tas.location
+
+  security_rule {
+    name                       = "ssh"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "http"
+    priority                   = 200
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+    
+    security_rule {
+    name                       = "https"
+    priority                   = 300
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "443"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_public_ip" "opsman_ip" {
+  name                = "opsman_ip"
+  resource_group_name = azurerm_resource_group.rg_tas.name
+  location            = azurerm_resource_group.rg_tas.location
+  allocation_method   = "Static"
+}
+
+resource "azurerm_network_interface" "opsman_nic" {
+  name                = "opsman_nic"
+  location            = azurerm_resource_group.rg_tas.location
+  resource_group_name = azurerm_resource_group.rg_tas.name
+
+  ip_configuration {
+    name                          = "opsmanipconf"
+    subnet_id                     = module.vnet.vnet_subnets[0]
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.opsman_ip.id
+  }
+}
+
+resource "azurerm_dns_a_record" "opsman_dns_record" {
+  name                = "opsman.${var.resource_group_name}"
+  zone_name           = "used4testing.xyz"
+  resource_group_name = "dns_jlarrea"
+  ttl                 = 300
+  records             = [data.azurerm_public_ip.opsman_ipaddress.ip_address]
+}
+
+
+
+## STORAGE ACCOUNTS ##
+
+resource "azurerm_storage_account" "opsman_storage" {
+  name                     = "${azurerm_resource_group.rg_tas.name}storage4tas"
+  resource_group_name      = azurerm_resource_group.rg_tas.name
+  location                 = azurerm_resource_group.rg_tas.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  allow_blob_public_access = true
+
+}
+
+resource "azurerm_storage_container" "opsman_container" {
+  name                  = "opsmanager"
+  storage_account_name  = azurerm_storage_account.opsman_storage.name
+}
+
+resource "azurerm_storage_container" "bosh_container" {
+  name                  = "bosh"
+  storage_account_name  = azurerm_storage_account.opsman_storage.name
+}
+
+resource "azurerm_storage_container" "stemcell_container" {
+  name                  = "stemcell"
+  storage_account_name  = azurerm_storage_account.opsman_storage.name
+  container_access_type = "blob"
+}
+
+resource "azurerm_storage_table" "opsman_table" {
+  name                 = "stemcells"
+  storage_account_name = azurerm_storage_account.opsman_storage.name
+}
+
+resource "azurerm_storage_account" "storage_accounts" {
+  for_each = toset( ["${azurerm_resource_group.rg_tas.name}storage4tas1", "${azurerm_resource_group.rg_tas.name}storage4tas2", "${azurerm_resource_group.rg_tas.name}storage4tas3" ] )
+  name                     = each.key
+  resource_group_name      = azurerm_resource_group.rg_tas.name
+  location                 = azurerm_resource_group.rg_tas.location
+  account_tier             = "Premium"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "bosh_containers" {
+  for_each = toset( ["${azurerm_resource_group.rg_tas.name}storage4tas1", "${azurerm_resource_group.rg_tas.name}storage4tas2", "${azurerm_resource_group.rg_tas.name}storage4tas3" ] )
+  name                  = "bosh"
+  storage_account_name  = each.key
+  depends_on = [azurerm_storage_account.storage_accounts]
+}
+
+resource "azurerm_storage_container" "stemcell_containers" {
+  for_each = toset( ["${azurerm_resource_group.rg_tas.name}storage4tas1", "${azurerm_resource_group.rg_tas.name}storage4tas2", "${azurerm_resource_group.rg_tas.name}storage4tas3" ] )
+  name                  = "stemcell"
+  storage_account_name  = each.key
+  depends_on = [azurerm_storage_account.storage_accounts]
+}
+
+resource "azurerm_storage_blob" "opsman_blob" {
+  name                   = "opsman.vhd"
+  storage_account_name   = azurerm_storage_account.opsman_storage.name
+  storage_container_name = azurerm_storage_container.opsman_container.name
+  type                   = "Page"
+  source_uri             = var.opsman_image_url
+}
+
+## VIRTUAL MACHINES ##
+
+resource "azurerm_image" "opsman-image" {
+  name                = "opsman-image"
+  location            = azurerm_resource_group.rg_tas.location
+  resource_group_name = azurerm_resource_group.rg_tas.name
+
+  os_disk {
+    os_type  = "Linux"
+    os_state = "Generalized"
+    blob_uri = azurerm_storage_blob.opsman_blob.url
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "opsman-vm" {
+  name                = "opsman-vm"
+  resource_group_name = azurerm_resource_group.rg_tas.name
+  location            = azurerm_resource_group.rg_tas.location
+  size                = "Standard_DS2_v2"
+  admin_username      = "ubuntu"
+  source_image_id     = azurerm_image.opsman-image.id
+  network_interface_ids = [
+    azurerm_network_interface.opsman_nic.id,
+  ]
+
+  admin_ssh_key {
+    username   = "ubuntu"
+    public_key = file("~/.ssh/azurekeys/opsman.pub")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+    disk_size_gb         = 128
+  }
+}
+
+
+#############################################################################
+# PROVISIONERS
+#############################################################################
+
+resource "null_resource" "opsman-config" {
+
+  provisioner "local-exec" {
+    command = <<EOT
+echo "export SP_SECRET=${azuread_service_principal_password.sp_for_tas.value}" >> next-step.txt
+echo "export OPSMAN_URL=${azurerm_dns_a_record.opsman_dns_record.name}.${azurerm_dns_a_record.opsman_dns_record.zone_name}" >> next-step.txt
+echo "export SUBSCRIPTION_ID=${data.azurerm_subscription.current.subscription_id}" >> next-step.txt
+echo "export TENANT_ID=${data.azurerm_subscription.current.tenant_id}" >> next-step.txt
+echo "export STORAGE_NAME=${azurerm_storage_account.opsman_storage.name}" >> next-step.txt
+EOT
+  }
+}
+
+#############################################################################
+# OUTPUTS
+#############################################################################
---
+++ run.sh
@@ -0,0 +1,324 @@
+#!/bin/bash
+
+az cloud set --name AzureCloud
+
+echo "Log in with your VMware AD account on your browser"
+az login
+
+read -p 'Please enter the Opsman exact version and build. 
+You can find that info here https://network.pivotal.io/products/ops-manager/#/releases (Example: 2.9.11-build.186). : ' OPSMAN_VERSION
+# read -p 'Please enter the TAS version you would like to install: ' TAS_VERSION
+# read -p 'Please enter your Pivnet API refresh token. If you do NOT have one: Log into pivnet > edit profile > request new refresh token). ' REFRESH_TOKEN
+read -p 'Which is your preferred location? (eastus or westus): ' EAST_WEST
+read -p 'Please enter a unique name for your resource group - all lowercase (Example: jsmith): ' RESOURCE_GROUP
+read -sp 'Please enter a new password for Opsman: ' OPSMAN_PASSWORD
+echo ''
+
+export TF_VAR_location=$EAST_WEST
+export TF_VAR_opsman_image_url="https://opsmanager$TF_VAR_location.blob.core.windows.net/images/ops-manager-$OPSMAN_VERSION.vhd"
+export TF_VAR_resource_group_name=$RESOURCE_GROUP
+export TF_VAR_sp_identifier="http://BoshAzure$RESOURCE_GROUP"
+export OPSMAN_URL="opsman.$RESOURCE_GROUP.used4testing.xyz"
+
+# Create a keypair
+if [ -d "$HOME/.ssh/azurekeys" ]; then
+    echo "Key pair already exists"
+else
+    echo "Creating a key pair in ~/.ssh/azurekeys"
+    mkdir ~/.ssh/azurekeys
+    ssh-keygen -t rsa -f ~/.ssh/azurekeys/opsman -C ubuntu -N ""
+fi
+
+terraform init
+terraform plan -out tas.tfplan
+terraform apply "tas.tfplan"
+
+source next-step.txt 
+rm next-step.txt 
+
+opsman_authentication_setup()
+{
+  cat <<EOF
+{
+    "setup": {
+    "decryption_passphrase": "$OPSMAN_PASSWORD",
+    "decryption_passphrase_confirmation": "$OPSMAN_PASSWORD",
+    "eula_accepted": "true",
+    "identity_provider": "internal",
+    "admin_user_name": "admin",
+    "admin_password": "$OPSMAN_PASSWORD",
+    "admin_password_confirmation": "$OPSMAN_PASSWORD"
+    }
+}
+EOF
+}
+
+curl -k -X POST -H "Content-Type: application/json" -d "$(opsman_authentication_setup)" "https://$OPSMAN_URL/api/v0/setup"
+
+echo "Setting up Opsman authentication..."
+sleep 60
+
+uaac target https://$OPSMAN_URL/uaa --skip-ssl-validation
+uaac token owner get opsman admin -s "" -p $OPSMAN_PASSWORD
+OPSMAN_TOKEN=$(uaac context | grep access_token | cut -c 21-)
+
+director_newconfig()
+{
+  cat <<EOF
+{
+  "director_configuration": {
+    "ntp_servers_string": "ntp.ubuntu.com",
+    "resurrector_enabled": false,
+    "director_hostname": null,
+    "max_threads": null,
+    "custom_ssh_banner": null,
+    "metrics_server_enabled": true,
+    "system_metrics_runtime_enabled": true,
+    "opentsdb_ip": null,
+    "director_worker_count": 5,
+    "post_deploy_enabled": false,
+    "bosh_recreate_on_next_deploy": false,
+    "bosh_director_recreate_on_next_deploy": false,
+    "bosh_recreate_persistent_disks_on_next_deploy": false,
+    "retry_bosh_deploys": false,
+    "keep_unreachable_vms": false,
+    "identification_tags": {},
+    "skip_director_drain": false,
+    "job_configuration_on_tmpfs": false,
+    "nats_max_payload_mb": null,
+    "database_type": "internal",
+    "blobstore_type": "local",
+    "local_blobstore_options": {
+      "enable_signed_urls": true
+    },
+    "hm_pager_duty_options": {
+      "enabled": false
+    },
+    "hm_emailer_options": {
+      "enabled": false
+    },
+    "encryption": {
+      "keys": [],
+      "providers": []
+    }
+  },
+  "dns_configuration": {
+    "excluded_recursors": [],
+    "recursor_selection": null,
+    "recursor_timeout": null,
+    "handlers": []
+  },
+  "security_configuration": {
+    "trusted_certificates": null,
+    "generate_vm_passwords": true,
+    "opsmanager_root_ca_trusted_certs": false
+  },
+  "syslog_configuration": {
+    "enabled": false
+  },
+  "iaas_configuration": {
+    "name": "default",
+    "additional_cloud_properties": {},
+    "subscription_id": "$SUBSCRIPTION_ID",
+    "tenant_id": "$TENANT_ID",
+    "client_id": "$TF_VAR_sp_identifier",
+    "client_secret": "$SP_SECRET",
+    "resource_group_name": "$RESOURCE_GROUP",
+    "bosh_storage_account_name": "$STORAGE_NAME",
+    "cloud_storage_type": "managed_disks",
+    "storage_account_type": "Premium_LRS",
+    "default_security_group": null,
+    "deployed_cloud_storage_type": null,
+    "deployments_storage_account_name": null,
+    "ssh_public_key": "$(cat ~/.ssh/azurekeys/opsman.pub)",
+    "ssh_private_key": "$(cat ~/.ssh/azurekeys/opsman | tr -d '\n')",
+    "environment": "AzureCloud",
+    "availability_mode": "availability_zones"
+  }
+}
+EOF
+}
+
+echo "Configuring bosh director..."
+curl -k -X PUT -H "Content-Type: application/json" -H "Authorization: Bearer $OPSMAN_TOKEN" -d "$(director_newconfig)" "https://$OPSMAN_URL/api/v0/staged/director/properties"
+
+networks_config()
+{
+  cat <<EOF
+{
+    "icmp_checks_enabled": false,
+    "networks": [
+      {
+        "guid": null,
+        "name": "infrastructure",
+        "subnets": [
+          {
+            "guid": null,
+            "iaas_identifier": "tas-virtual-network/tas-infrastructure",
+            "cidr": "10.0.4.0/26",
+            "dns": "168.63.129.16",
+            "gateway": "10.0.4.1",
+            "reserved_ip_ranges": "10.0.4.1-10.0.4.9"
+          }
+        ]
+      },
+      {
+        "guid": null,
+        "name": "tas",
+        "subnets": [
+          {
+            "guid": null,
+            "iaas_identifier": "tas-virtual-network/tas-runtime",
+            "cidr": "10.0.12.0/22",
+            "dns": "168.63.129.16",
+            "gateway": "10.0.12.1",
+            "reserved_ip_ranges": "10.0.12.1-10.0.12.9"
+          }
+        ]
+      }, 
+      {
+        "guid": null,
+        "name": "services",
+        "subnets": [
+          {
+            "guid": null,
+            "iaas_identifier": "tas-virtual-network/tas-services",
+            "cidr": "10.0.8.0/22",
+            "dns": "168.63.129.16",
+            "gateway": "10.0.8.1",
+            "reserved_ip_ranges": "10.0.8.1-10.0.8.9"
+          }
+        ]
+      } 
+    ]
+  }
+EOF
+}
+
+curl -k -X PUT -H "Content-Type: application/json" -H "Authorization: Bearer $OPSMAN_TOKEN" -d "$(networks_config)" "https://$OPSMAN_URL/api/v0/staged/director/networks"
+
+az_singleton()
+{
+  cat <<EOF
+{
+  "network_and_az": {
+    "network": {
+      "name": "infrastructure"
+    },
+    "singleton_availability_zone": {
+      "name": "zone-1"
+    }
+  }
+}
+EOF
+}
+
+curl -k -X PUT -H "Content-Type: application/json" -H "Authorization: Bearer $OPSMAN_TOKEN" -d "$(az_singleton)" "https://$OPSMAN_URL/api/v0/staged/director/network_and_az"
+
+# echo "Retrieving Tanzu Network access token..."
+# generate_pivnet_token()
+# {
+# cat <<EOF
+# {"refresh_token":"$REFRESH_TOKEN"}
+# EOF
+# }
+
+# PIVNET_TOKEN=$(curl -sX POST https://network.pivotal.io/api/v2/authentication/access_tokens -d "$(generate_pivnet_token)" | jq -r '.access_token')
+
+# echo "Creating a product download link..."
+# RELEASE_ID=$(curl -sX GET https://network.pivotal.io/api/v2/products/elastic-runtime/releases -H "Authorization: Bearer $PIVNET_TOKEN" |jq -r --arg TAS_VERSION "$TAS_VERSION" '.[] | .[] | select(.version==$TAS_VERSION) | .id')
+
+# PRODUCT_FILE_URL=$(curl -sX GET "https://network.pivotal.io/api/v2/products/elastic-runtime/releases/$RELEASE_ID/product_files" -H "Authorization: Bearer $PIVNET_TOKEN" | jq -r '.[] | .[] | select(.name=="Pivotal Application Service") | ._links.download.href')
+
+# echo "Downloading TAS..."
+# DOWNLOAD_LINK=$(curl -sX GET $PRODUCT_FILE_URL -H "Authorization: Bearer $PIVNET_TOKEN" | awk '{ print substr ($0, 36, length($0) - 66 ) }' | sed 's/amp;//g')
+
+# ssh -q -o StrictHostKeyChecking=no -i ~/.ssh/azurekeys/opsman ubuntu@$OPSMAN_URL "cat << EOF > /home/ubuntu/download_tas.sh 
+# curl -X GET '$DOWNLOAD_LINK' -o tas-tile.pivotal
+# EOF"
+
+# ssh -q -o StrictHostKeyChecking=no -i ~/.ssh/azurekeys/opsman ubuntu@$OPSMAN_URL 'bash /home/ubuntu/download_tas.sh'
+
+# echo "Uploading TAS to Opsman...this could take up to an hour..."
+# ssh -q -o StrictHostKeyChecking=no -i ~/.ssh/azurekeys/opsman ubuntu@$OPSMAN_URL "curl -k "https://$OPSMAN_IP/api/v0/available_products" -X POST -H 'Authorization: Bearer $OPSMAN_TOKEN' -F 'product[file]=@/home/ubuntu/tas-tile.pivotal'"
+
+# echo "TAS upload completed..."
+
+
+# stage_product()
+# {
+#   cat <<EOF
+# {"name": "cf",
+# "product_version": "$TAS_VERSION"}
+# EOF
+# }
+
+# echo "Staging TAS..."
+# curl -k -X POST -H "Content-Type: application/json" -H "Authorization: Bearer $OPSMAN_TOKEN" -d "$(stage_product)" "https://$OPSMAN_URL/api/v0/staged/products"
+
+# echo "Downloading Stemcell..."
+# STEMCELL_ID=$(curl -sX GET "https://network.pivotal.io/api/v2/products/elastic-runtime/releases/$RELEASE_ID/dependencies" -H "Authorization: Bearer $PIVNET_TOKEN" | jq -r '.[] | .[] | .[] | select(.product.slug=="stemcells-ubuntu-xenial") | .id' 2>/dev/null | sed 1q)
+
+# STEMCELL_VERSION=$(curl -sX GET "https://network.pivotal.io/api/v2/products/elastic-runtime/releases/$RELEASE_ID/dependencies" -H "Authorization: Bearer $PIVNET_TOKEN" | jq -r '.[] | .[] | .[] | select(.product.slug=="stemcells-ubuntu-xenial") | .version' 2>/dev/null | sed 1q)
+
+# STEMCELL_PRODUCT_URL=$(curl -sX GET "https://network.pivotal.io/api/v2/products/stemcells-ubuntu-xenial/releases/$STEMCELL_ID/product_files" -H "Authorization: Bearer $PIVNET_TOKEN" | jq -r '.[] | .[] | select(.name | contains("Azure") ) | ._links.download.href' 2>/dev/null)
+
+# STEMCELL_DOWNLOAD_LINK=$(curl -sX GET $STEMCELL_PRODUCT_URL -H "Authorization: Bearer $PIVNET_TOKEN" | awk '{ print substr ($0, 36, length($0) - 66 ) }' | sed 's/amp;//g')
+
+# ssh -q -o StrictHostKeyChecking=no -i ~/.ssh/azurekeys/opsman ubuntu@$OPSMAN_URL "cat << EOF > /home/ubuntu/download_stemcell.sh 
+# curl -O -J -X GET '$STEMCELL_DOWNLOAD_LINK'
+# EOF"
+
+# ssh -q -o StrictHostKeyChecking=no -i ~/.ssh/azurekeys/opsman ubuntu@$OPSMAN_URL 'bash /home/ubuntu/download_stemcell.sh'
+
+# echo "Uploading Stemcell to Opsman..."
+
+# ssh -q -o StrictHostKeyChecking=no -i ~/.ssh/azurekeys/opsman ubuntu@$OPSMAN_URL "curl -k "https://$OPSMAN_IP/api/v0/stemcells" -X POST -H 'Authorization: Bearer $OPSMAN_TOKEN' -F 'stemcell[file]=@/home/ubuntu/bosh-stemcell-$STEMCELL_VERSION-azure-hyperv-ubuntu-xenial-go_agent.tgz' -F 'stemcell[floating]=false'"
+
+# echo "Stemcell upload completed..."
+
+# CF_GUID=$(curl -k -X GET https://$OPSMAN_URL/api/v0/staged/products -H "Authorization: Bearer $OPSMAN_TOKEN" | jq -r '.[] | select(.type=="cf") | .guid')
+
+# associate_stemcell()
+# {
+#   cat <<EOF
+# {
+#   "products": [
+#     {
+#       "guid": "$CF_GUID",
+#       "staged_stemcells": [
+#         {
+#           "os": "ubuntu-xenial",
+#           "version": "$STEMCELL_VERSION"
+#         }
+#       ]
+#     }
+#   ]
+# }
+# EOF
+# }
+
+# curl -k -X PATCH -H "Content-Type: application/json" -H "Authorization: Bearer $OPSMAN_TOKEN" -d "$(associate_stemcell)" "https://$OPSMAN_URL/api/v0/stemcell_associations"
+
+# echo "Stemcell associated with TAS..."
+
+apply_changes()
+{
+  cat <<EOF
+{
+"deploy_products": "all",
+"ignore_warnings": true
+}
+EOF
+}
+
+curl -k -X POST -H "Content-Type: application/json" -H "Authorization: Bearer $OPSMAN_TOKEN" -d "$(apply_changes)" "https://$OPSMAN_URL/api/v0/installations"
+
+echo "
+
+Apply changes to deploy Bosh is currently running. 
+You Opsman URL is $OPSMAN_URL
+Your username is admin
+Your password is $OPSMAN_PASSWORD
+ssh to Opsman vm:
+ssh -o StrictHostKeyChecking=no -i ~/.ssh/azurekeys/opsman ubuntu@$OPSMAN_URL"
