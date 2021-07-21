cdb36f959a6bcc9878dda77f0a0490e22df32771
pivotal-james-larrea <jlarrea@pivotal.io>
2021-07-21 11:39:38 -0400

    added login command

--- clean_up.sh
+++ clean_up.sh
@@ -6,7 +6,7 @@ read -p 'Please enter the name of the resource group you used to deploy TAS: ' R
 DOMAIN="used4testing.xyz"
 DNS_ZONE="dns_jlarrea"
 
-
+az login
 az group delete -n $RESOURCE_GROUP -y
 az ad sp delete --id http://BoshAzure$RESOURCE_GROUP
 az network dns record-set a delete -n "*.apps.$RESOURCE_GROUP" -z $DOMAIN -g $DNS_ZONE -y
